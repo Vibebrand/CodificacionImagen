d6ac050b4c14ff61b03caf2418474597d228affa
Luis Alejandro Rangel Sánchez <xlarsx@gmail.com>
2013-02-04 17:16:33 -0600

    Corregida codificacion

--- CodificacionImagen/ImageViewController.m
+++ CodificacionImagen/ImageViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ImageViewController.h"
+#import "GTMBase64.h"
 
 @interface ImageViewController ()
 
@@ -27,6 +28,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self.urlImage setText: @"http://192.168.100.23:8000/1.png"];
     [self.urlImage setDelegate: self];
     [self.textArea setText: @""];
 }
@@ -42,14 +44,14 @@
     NSString * urlImageText = [self.urlImage text];
     NSURL *url = [NSURL URLWithString: urlImageText];
     NSData *data = [NSData dataWithContentsOfURL:url];
-    UIImage *img = [[[UIImage alloc] initWithData:data scale: 1.0] autorelease];
+    
+    NSString * sdata = [self serialize: data];
+    NSData * dataRes = [self deserialize: sdata];
     
     
-    NSData * pictureData = UIImagePNGRepresentation(img);
-    NSString * pictureDataString = [self encodeBase64: pictureData];
-    
+    UIImage *img = [[[UIImage alloc] initWithData:dataRes scale: 1.0] autorelease];
     [self.imageView setImage: img];
-    [self.textArea setText: pictureDataString];
+    [self.textArea setText: sdata];
     [self.view endEditing: YES];
     
     return NO;
@@ -62,62 +64,18 @@
     
     return YES;
 }
+-(NSString *) serialize:(NSData*)data {
+    
+    return [GTMBase64 stringByEncodingData:data];
+}
 
-- (NSString*)encodeBase64: (NSData * ) data {
-    static char* alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+/**
+ * Deserialize Base64 NSString to NSData
+ */
+-(NSData*) deserialize: (NSString *) input {
     
-    unsigned int length = data.length;
-    unsigned const char* rawData = data.bytes;
+    return [GTMBase64 decodeString: input];
     
-    //empty data = empty output
-    if (length == 0) {
-        return @"";
-    }
-    
-    unsigned int outputLength = (((length + 2) / 3) * 4);
-    
-    //let's allocate buffer for the output
-    char* rawOutput = malloc(outputLength + 1);
-    
-    //with each step we get 3 bytes from the input and write 4 bytes to the output
-    for (unsigned int i = 0, outputIndex = 0; i < length; i += 3, outputIndex += 4) {
-        BOOL triple = NO;
-        BOOL quad = NO;
-        
-        //get 3 bytes (or only 1 or 2 when we have reached the end of input)
-        unsigned int value = rawData[i];
-        value <<= 8;
-        
-        if (i + 1 < length) {
-            value |= rawData[i + 1];
-            triple = YES;
-        }
-        
-        value <<= 8;
-        
-        if (i + 2 < length) {
-            value |= rawData[i + 2];
-            quad = YES;
-        }
-        
-        //3 * 8 bits written as 4 * 6 bits (indexing the 64 chars of the alphabet)
-        //write = if end of input reached
-        rawOutput[outputIndex + 3] = (quad) ? alphabet[value & 0x3F] : '=';
-        value >>= 6;
-        rawOutput[outputIndex + 2] = (triple) ? alphabet[value & 0x3F] : '=';
-        value >>= 6;
-        rawOutput[outputIndex + 1] = alphabet[value & 0x3F];
-        value >>= 6;
-        rawOutput[outputIndex] = alphabet[value & 0x3F];
-    }
-    
-    rawOutput[outputLength] = 0;
-    
-    NSString* output = [NSString stringWithCString:rawOutput encoding:NSASCIIStringEncoding];
-    
-    free(rawOutput);
-    
-    return output;
 }
 
 - (void)dealloc {
